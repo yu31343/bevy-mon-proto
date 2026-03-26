05f94f7ea116e0fda8e61f4b33728e98f4303ff0
ChymieOwO <cmiowo@stu.xjtu.edu.com>
2026-03-26 23:33:09 +0800

    Test

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//! A minimal example that outputs "hello world"
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new().add_systems(Update, hello_world_system).run();
+}
+
+fn hello_world_system()
+{
+    println!("hello world!");
+}
